52ff5df7ba2116c0715a9c02f55909f3726c19a2
Henri Maronen <henri.maronen@slush.org>
2025-06-27 10:11:07 +0300

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['@radix-ui/react-icons'],
+  },
+}
+
+export default nextConfig
